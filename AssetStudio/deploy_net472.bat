b1ac7112fb3a6ee44f5a1d16c535e7b868dfd006
simalaoshi <1742495214@qq.com>
2022-09-01 10:39:50 +0800

    1，增加部署脚本

--- AssetStudio/deploy_net472.bat
+++ AssetStudio/deploy_net472.bat
@@ -1,8 +1,9 @@
 pyinstaller.exe -F pkg.py -n script.exe
-
-set mydate=%date:/=%
-set TIMESTAMP=%mydate%
-set OUTPUT=pkg-doctor-net472-%TIMESTAMP%
+set fname=%date:~2,2%%date:~5,2%%date:~8,2%
+set fname=%fname: =0%
+rem set mydate=%date:/=%
+rem set TIMESTAMP=%mydate%
+set OUTPUT=pkg-doctor-net472-%fname%
 rmdir /S /Q %OUTPUT%
 mkdir %OUTPUT%
 set SRC=AssetStudioGUI/bin/Release/net472
